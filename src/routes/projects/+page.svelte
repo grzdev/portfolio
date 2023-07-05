84850167e78a1f92e9d93304a8a5bdbb8ad4dcdd
zay <damilolaoyeniyi13@gmail.com>
2023-07-05 10:33:14 +0100

    Mobile UI change

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,24 @@
+<!-- javascript -->
+<script>
+  // imports
+  import LoadScreen from "../../components/LoadScreen.svelte";
+  import Navbar from "../../components/Navbar.svelte";
+  import Projects from "../../components/Projects.svelte";
+  let menuOpen = false;
+
+  // LoadScreen
+  let isLoading = true;
+  setTimeout(() => {
+    isLoading = false;
+  }, 2000);
+</script>
+
+<!-- html -->
+{#if isLoading}
+  <LoadScreen />
+{:else}
+  <div>
+    <Navbar {menuOpen} on:toggleMenu={() => (menuOpen = !menuOpen)} />
+    <Projects />
+  </div>
+{/if}
